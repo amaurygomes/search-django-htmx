7d95725ad68ede624d2f41ea6d3c0082a61d0eb2
Amaury Gomes Ibanez <amaury_mju@hotmail.com>
2024-04-21 14:29:58 -0400

    Update

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fhi1v8d96(-!k^3x6i*c1ha2vz@4r%hdi-l$2(@7v1!wzr_4%*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
